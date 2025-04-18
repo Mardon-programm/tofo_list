1b752e639913a7a7674b0b64faed2a18acfc5e9b
Mardon-programm <creatermari@gmail.com>
2025-04-18 14:43:20 +0600

    исправил ошибки в админ юрл

--- todoproject/urls.py
+++ todoproject/urls.py
@@ -20,8 +20,6 @@ from rest_framework.routers import DefaultRouter
 from todos.views import TodoViewSet
 from users.views import UserCreateAPIView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from django.conf import settings
-from django.conf.urls.static import static
 
 router = DefaultRouter()
 router.register(r'todos', TodoViewSet, basename='todo')
@@ -32,4 +30,4 @@ urlpatterns = [
     path('api/users/register/', UserCreateAPIView.as_view()),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
