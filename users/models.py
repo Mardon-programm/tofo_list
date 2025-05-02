eeb31b89e28b0594cef4cd6fdaa4c1b84303b418
Mardon-programm <creatermari@gmail.com>
2025-05-02 18:20:29 +0600

    Исправление ошибок

--- users/models.py
+++ users/models.py
@@ -3,8 +3,11 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class User(AbstractUser):
+    username = models.CharField(max_length=100, unique=True)
     email = models.EmailField(unique=True)
     phone_number = models.CharField(max_length=13, unique=True)
+    created_at = models.DateField(auto_now_add=True)
+    age = models.PositiveIntegerField(null=True, blank=True)
 
 
     def __str__(self):
